7776354e1b4332b2a8142b2e751505258771af3c
João Gularte <107907952+JoaoGularteoqmc@users.noreply.github.com>
2024-06-27 10:22:39 -0300

    menus

--- pedido.py
+++ pedido.py
@@ -1,3 +1,73 @@
-adicionar
-listar
-remover
+class Pedido:
+    def __init__(self, codigo, quantidade):
+        self.codigo = codigo
+        self.quantidade = quantidade
+
+"""""
+    def adicionar_prod
+    def remover_prod
+    def listar_ped
+
+"""""
+
+"""""
+def menu_cadastro(): 
+    print("\n=== Menu ===")
+    print("1) Realizar Cadastro")
+    print("2) Informar Cadastro")
+    print("0) Sair")
+
+while True:
+    menu_cadastro()
+    opcao = input("Selecione uma opção: ")
+
+    if opcao == "1":
+        
+    elif opcao == "0":
+        break
+    else:
+        print("Opção inválida.")
+"""""
+
+"""""
+def menu_user(): 
+    print("\n=== Menu ===")
+    print("1) Adicionar Produto")
+    print("2) Remover Produto")
+    print("2) Listar Pedido")
+    print("0) Sair")
+
+while True:
+    menu_user()
+    opcao = input("Selecione uma opção: ")
+
+    if opcao == "1":
+        
+    elif opcao == "0":
+        break
+    else:
+        print("Opção inválida.")
+"""""
+
+"""""
+def menu_admin(): 
+    print("\n=== Menu ===")
+    print("1) Visualizar Estoque")
+    print("2) Catalogar Produto")
+    print("0) Sair")
+
+while True:
+    menu_admin()
+    opcao = input("Selecione uma opção: ")
+
+    if opcao == "1":
+        
+    elif opcao == "0":
+        break
+    else:
+        print("Opção inválida.")
+"""""
+
+
+
+
