3db22d219d434b27f1a17311e56b3549f692e2a1
kau <magnabosco.kka03@gmail.com>
2024-07-01 22:23:38 -0300

    Menu administrador completo e funcionando, classe Produto em dia

--- Produto.py
+++ Produto.py
@@ -1,4 +1,4 @@
-lista_estoque = ["oi"]
+lista_estoque = []
 
 
 class Produto:
@@ -7,62 +7,73 @@ class Produto:
         self.nome = nome
         self.preco = preco
         
+    def quantidade_recebida(self, quantidade):
+        for x in range (quantidade):
+            lista_estoque.append(self)
+    
+        
 class Agua(Produto):
-    def __init__(self, codigo, nome, preco, ml, gaseificada):
+    def __init__(self, codigo, nome, preco, ml):
         super().__init__(codigo, nome, preco)
         self.ml = ml
-        self.gaseificada = gaseificada #booleano
-        
+    
 class Pao(Produto):
-    def __init__(self, codigo, nome, preco, salgado, integral):
+    def __init__(self, codigo, nome, preco, salgado):
         super().__init__(codigo, nome, preco)
-        self.salgado = salgado #booleano
-        self.integral = integral #booleano
+        self.salgado = salgado 
         
 class Racao(Produto):
     def __init__(self, codigo, nome, preco, tipo_animal):
         super().__init__(codigo, nome, preco)
         self.tipo_animal = tipo_animal
         
-# Simula o rebebimento do produto pelo mercado
-def receber_produto(self, prod ):
-    if prod == 1:
-        cod = 1010
-        nome = "Água"
-        ml = input("500 ou 1000 Ml? ")
-        gaseificada = input("Com ou sem gás? ")
-        if ml == "500":
-            preco = self.preco
-        elif ml == "1000":
-            preco = self.preco + 2.0
-        agua = Agua(cod, nome, preco, ml, gaseificada)
-        return lista_estoque.append(agua)
-    else:
-        print("OH yeah")
         
+def receber_produto( prod ):
+        if prod == "Água":
+            ml = input("Preferência de 500 ou 1000 Ml? \n500 ML = 1 \n1000 Ml = 2 \nEscolha: ")
+            if ml == "1":
+                nome = "Água 500Ml"
+                cod = 1010
+                preco = 5.0
+            elif ml == "2":
+                nome = "Água 1000Ml"
+                cod = 2020
+                preco = 10.0
+            agua = Agua(cod, nome, preco, ml)
+            return agua
+        elif prod == "Pão":
+            sal = input("Preferência de Pão doce ou salgado? \nSalgado = 1 \nDoce = 2 \nEscolha: ")
+            if sal == "1":
+                nome = "Pão Salgado"
+                cod = 1415
+                preco = 2.0
+            elif sal == "2":
+                nome = "Pão Doce"
+                cod = 1515
+                preco = 3.0
+            pao = Pao(cod, nome, preco, sal)
+            return pao
+        else:
+            print("Produto Inválido!")
+        ## Outros produtos
         
-def imprimir_lista():
-    print(lista_estoque)
-        
-# def cadastrar_produto(self, produto):
-#     if produto == "Água":
-        
-
-def atualizar_preco(self, novo_preco):
-    if novo_preco <= 0:
-        print("O preço do produto não pode ser negativo.")
-    else:
-        self.preco = float(novo_preco)
+    
+    
 
 
-######################################################
 
-    def __str__(self):
-        texto = "\nCódigo: " + str(self.codigo) + "\n"
-        texto += "Nome: " + self.nome + "\n"
-        texto += "Preço: " + str(self.preco) + "\n"
-        texto += "Quantidade_estoque: " + str(self.quantidade) + "\n"
-        return texto
 
-    def imprimir(self):  
-        print(self)  
+# def atualizar_preco(self, novo_preco):
+#     if novo_preco <= 0:
+#         print("O preço do produto não pode ser negativo.")
+#     else:
+#         self.preco = float(novo_preco)
+
+    # def __str__(self):
+    #     texto = "\nCódigo: " + str(self.codigo) + "\n"
+    #     texto += "Nome: " + self.nome + "\n"
+    #     texto += "Preço: " + str(self.preco) + "\n"
+    #     return texto
+
+    # def imprimir(self):  
+    #     print(self)  
--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 # Importando o arquivo da classe Produto
-from Produto import Produto, receber_produto
+from Produto import Produto, receber_produto, lista_estoque
 
 # Criando o menu principal
 menu_principal_string = """
@@ -53,10 +53,42 @@ def menu_administrador():
         if menu == "0":
             break 
         elif menu == "1":
-            pass #visualisar estoque
+            for produto in lista_estoque:
+                print(f"Nome: {produto.nome} \nCódigo: {produto.codigo} \n ----- ")
         elif menu == "2":
-            receber_produto(1)
+            menu_cadastro_produtos()
         else:
             input(f"Valor {menu} inválido, tente novamente (Enter) ")
             continue
-       
+'''
+objeto = receber_produto("Água")
+objeto.quantidade_recebida(int(input(f"Defina a quantidade desejada do produto {objeto.nome}: ")))
+'''
+menu_cadastro_produtos_string = """
+==================
+Menu Cadastro Produtos
+==================
+0 - Sair
+1 - Água
+2 - Pão
+3 - Ração
+"""
+
+def menu_cadastro_produtos():
+    while True:
+        print(menu_cadastro_produtos_string)
+        menu = input("Escolha: ")
+        if menu == "0":
+            break 
+        elif menu == "1":
+            objeto = receber_produto("Água")
+            objeto.quantidade_recebida(int(input(f"Defina a quantidade desejada do produto {objeto.nome}: ")))
+        elif menu == "2":
+            objeto = receber_produto("Pão")
+            objeto.quantidade_recebida(int(input(f"Defina a quantidade desejada do produto {objeto.nome}: ")))
+        elif menu == "3":
+            pass
+        else:
+            # Caso o valor seja inválido, (continue) não quebra o loop
+            input(f"Valor {menu} inválido, tente novamente (Enter) ")
+            continue
